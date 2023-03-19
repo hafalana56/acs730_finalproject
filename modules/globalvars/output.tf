18f3d846709685324dfed038b4d7559da4f0097d
Hafees Falana <ec2-user@ip-172-31-24-181.ec2.internal>
2023-03-19 05:44:40 +0000

    modules updated

---
+++ modules/globalvars/output.tf
@@ -0,0 +1,15 @@
+output "default_tags" {
+  value = {
+    "Owner"   = "Group0"
+    "Project" = "ACS730 Final Project"
+  }
+}
+
+
+output "region" {
+  value = "us-east-1"
+}
+
+output "prefix" {
+  value = "Group0"
+}
